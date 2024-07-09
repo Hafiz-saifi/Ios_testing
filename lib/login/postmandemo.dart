9aeee274a9a2103fc8cd4ccb642ccfe89261c1e4
unknown <hafizbakar1248@gmail.com>
2024-07-09 17:02:42 +0500

    second commit

--- lib/login/postmandemo.dart
+++ lib/login/postmandemo.dart
@@ -74,6 +74,24 @@ class LoginScreen extends StatelessWidget {
                     } catch (e) {
                       // Handle error, e.g., show error message
                       print('object');
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Login Successful'),
+                            content: Text('Token'),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text('OK'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  // Navigate to another screen or perform other actions
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     }
                   },
                   child: Text('Login',style: TextStyle(color: Colors.white),),
